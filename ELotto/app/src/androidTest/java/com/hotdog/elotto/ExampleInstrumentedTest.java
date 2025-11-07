3a2e8c2f7cb1f2be08b1db5bcb276246fae9a4ef
dzhong4 <dzhong4@ualberta.ca>
2025-11-07 16:32:11 -0700

    Edited the default test

--- ELotto/app/src/androidTest/java/com/hotdog/elotto/ExampleInstrumentedTest.java
+++ ELotto/app/src/androidTest/java/com/hotdog/elotto/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.elotto", appContext.getPackageName());
+        assertEquals("com.hotdog.elotto", appContext.getPackageName());
     }
 }
