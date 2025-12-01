a4cee38011dca1e611246798401de33aa6f4d000
ethancarter7 <ethancarter72005@gmail.com>
2025-11-30 21:30:25 -0700

    fixed some old tests (method/variable names changed)

--- ELotto/app/src/androidTest/java/com/hotdog/elotto/ui/CreateEventInputIntentTest.java
+++ ELotto/app/src/androidTest/java/com/hotdog/elotto/ui/CreateEventInputIntentTest.java
@@ -46,15 +46,15 @@ public class CreateEventInputIntentTest {
         intending(hasAction(Intent.ACTION_GET_CONTENT)).respondWith(result);
 
         onView(withId(R.id.Event_Poster_Input)).perform(click());
-        onView(withId(R.id.Event_Name_Input)).perform(scrollTo(),typeText("Jesse We Need To Cook"), closeSoftKeyboard());
+        onView(withId(R.id.EventNameInput)).perform(scrollTo(),typeText("Jesse We Need To Cook"), closeSoftKeyboard());
         onView(withId(R.id.Event_Description_Input)).perform(scrollTo(),typeText("Blue"), closeSoftKeyboard());
-        onView(withId(R.id.Event_Price_Input)).perform(scrollTo(),typeText("$99.1"), closeSoftKeyboard());
-        onView(withId(R.id.Event_Location_Input)).perform(scrollTo(),typeText("Walter White"), closeSoftKeyboard());
-        onView(withId(R.id.Time_Input)).perform(scrollTo(),typeText("12:00"), closeSoftKeyboard());
-        onView(withId(R.id.Date_Input)).perform(scrollTo(),typeText("11/07/2025"), closeSoftKeyboard());
-        onView(withId(R.id.Open_Period_Input)).perform(scrollTo(),typeText("11/01/2025"), closeSoftKeyboard());
-        onView(withId(R.id.Close_Period_Input)).perform(scrollTo(),typeText("11/06/2025"), closeSoftKeyboard());
-        onView(withId(R.id.Entrant_Limit_Input)).perform(scrollTo(),typeText("50"), closeSoftKeyboard());
+        onView(withId(R.id.EventPriceInput)).perform(scrollTo(),typeText("$99.1"), closeSoftKeyboard());
+        onView(withId(R.id.EventAddressInput)).perform(scrollTo(),typeText("Walter White"), closeSoftKeyboard());
+        onView(withId(R.id.EventTimeInput)).perform(scrollTo(),typeText("12:00"), closeSoftKeyboard());
+        onView(withId(R.id.EventDateInput)).perform(scrollTo(),typeText("11/07/2025"), closeSoftKeyboard());
+        onView(withId(R.id.EventOpensSelector)).perform(scrollTo(),typeText("11/01/2025"), closeSoftKeyboard());
+        onView(withId(R.id.EventClosesSelector)).perform(scrollTo(),typeText("11/06/2025"), closeSoftKeyboard());
+        onView(withId(R.id.MaxEntrantInput)).perform(scrollTo(),typeText("50"), closeSoftKeyboard());
         onView(withId(R.id.Confirm_Creation_Button)).perform(click());
         onView(withText("Event Created")).check(matches(isDisplayed()));
 
